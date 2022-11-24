db9271d4cb4179549bddfca5dd2e8ed077b2ee4a
alinonet <101158367+alinonet@users.noreply.github.com>
2022-11-24 09:17:43 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/alinonet/n5geh.platform
+git clone https://github.com/alinonet/n5geh.entirety
+mv n5geh.entirety entirety
+chmod +x n5geh.platform/scripts/installation_setup.sh
+./n5geh.platform/scripts/installation_setup.sh
+echo Press Enter to reboot...
+read enter
