09ef7721abdeab54e042d5782314038515d4fc32
alinonet <101158367+alinonet@users.noreply.github.com>
2022-11-24 11:14:45 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 sudo sysctl -w vm.max_map_count=262144
 
-docker compose -f n5geh.platform/docker-compose.yml -f entirety/docker/docker-compose.yml up -d
+cd platform
+docker-compose up -d
+cd
+cd entirety/docker
+docker compose up -d
+
 docker compose logs -f
