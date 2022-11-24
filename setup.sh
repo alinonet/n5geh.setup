720ba4d89707e8f8c974ed0eaaac9a4891f8abd6
alinonet <101158367+alinonet@users.noreply.github.com>
2022-11-24 11:15:45 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
-cp *.sh ../
-cd ..
-mv n5geh.setup.entirety build
-rm build/*.sh
-
-sudo apt update
-sudo apt upgrade
-
+mv . n5geh
 git clone https://github.com/alinonet/n5geh.platform
+mv n5geh.platform platform
 git clone https://github.com/alinonet/n5geh.entirety
 mv n5geh.entirety entirety
-
-chmod +x n5geh.platform/scripts/installation_setup.sh
-./n5geh.platform/scripts/installation_setup.sh
-
+chmod +x platform/scripts/installation_setup.sh
+cd platform/scripts
+installation_setup.sh
 echo Press Enter to reboot...
 read enter
 sudo reboot
