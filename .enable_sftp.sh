bd8912948e916d11c1f0b763fffa9a9d8b8c946a
alinonet <101158367+alinonet@users.noreply.github.com>
2022-11-25 09:00:02 +0100

    Update .enable_sftp.sh

--- .enable_sftp.sh
+++ .enable_sftp.sh
@@ -1,18 +1,25 @@
-#!\bin\bash
+#!/bin/bash
 
+# Install SSH (if needed)
 # sudo apt install ssh -y
 # sudo systemctl enable ssh
 # sudo systemctl start ssh
 # sudo systemctl status ssh
 
-sudo addgroup sftp
-sudo useradd $USER
-sudo usermod -a -G sftp $USER
+# Create and add the current user to group "sftp" (if using the alternative way)
+# sudo addgroup sftp
+# sudo useradd $USER
+# sudo usermod -a -G sftp $USER
 
-sudo echo Match User $USER>>/etc/ssh/sshd_config
-sudo echo ChrootDirectory /var/sftp>>/etc/ssh/sshd_config
-sudo echo X11Forwarding no>>/etc/ssh/sshd_config
-sudo echo AllowTcpForwarding no>>/etc/ssh/sshd_config
-sudo echo ForceCommand internal-sftp>>/etc/ssh/sshd_config
-
+echo Copy and paste the following lines at the end of /etc/ssh/sshd_config:
+echo
+echo Match User $USER # echo Match Group sftp # Alternative permission by group
+echo ChrootDirectory /
+echo X11Forwarding no
+echo AllowTcpForwarding no
+echo ForceCommand internal-sftp
+echo
+echo Press Enter to open /etc/ssh/sshd_config and exit with CTRL+S, CTRL+X...
+read key
+sudo nano /etc/ssh/sshd_config
 sudo systemctl restart ssh
